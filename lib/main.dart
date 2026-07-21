d9e0917ecbadd1e3fc8f1d8567dbc28111b3d789
bf2012-bf <boyfriend2012i@gmail.com>
2026-07-20 20:19:45 -0500

    agregue el padding

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,12 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Directorio IST La Troncal"),
+          title: Center(child: Text("Directorio IST La Troncal")),
           backgroundColor: Colors.blue,
           foregroundColor: Colors.amber,
         ),
         body: ListView(
+        padding: EdgeInsets.all(10),
           children: [
             ListTile(
               trailing: Icon(Icons.star, color:  Color.fromARGB(255, 185, 194, 211)),
@@ -25,18 +26,21 @@ class MainApp extends StatelessWidget {
               title: Text("Ing. Israel Zurita"),
               subtitle: Text("Desarrollo de Aplicaciones Web"),
             ),
+            Divider(),
             ListTile(
               trailing: Icon(Icons.star, color:  Color.fromARGB(255, 185, 194, 211)),
               leading: Icon(Icons.person, color: Colors.blue),
               title: Text("Ing. Angel Novillo"),
               subtitle: Text("Desarrollo de Aplicaciones Web"),
             ),
+            Divider(),
             ListTile(
               trailing: Icon(Icons.star, color:  Color.fromARGB(255, 185, 194, 211)),
               leading: Icon(Icons.person, color: Colors.blue),
               title: Text("Ing. Cecilia Naula"),
               subtitle: Text("Desarrollo de Aplicaciones Web"),
             ),
+            Divider(),
           ],
         ),
       ),
