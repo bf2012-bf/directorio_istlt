cf5b25867a235400438b920f117070ddfa726aad
bf2012-bf <boyfriend2012i@gmail.com>
2026-07-20 20:06:09 -0500

    agrega listview y listtile

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("Directorio IST La Troncal"),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.amber,
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              trailing: Icon(Icons.star, color:  Color.fromARGB(255, 185, 194, 211)),
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text("Ing. Israel Zurita"),
+              subtitle: Text("Desarrollo de Aplicaciones Web"),
+            ),
+            ListTile(
+              trailing: Icon(Icons.star, color:  Color.fromARGB(255, 185, 194, 211)),
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text("Ing. Angel Novillo"),
+              subtitle: Text("Desarrollo de Aplicaciones Web"),
+            ),
+            ListTile(
+              trailing: Icon(Icons.star, color:  Color.fromARGB(255, 185, 194, 211)),
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text("Ing. Cecilia Naula"),
+              subtitle: Text("Desarrollo de Aplicaciones Web"),
+            ),
+          ],
         ),
       ),
     );
